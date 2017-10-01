9494dbd936a566c3273ee6da1c99eb18de570cb7
EugeFranchi <eugefranx@gmail.com>
2017-10-01 18:29:55 -0300

    Update hash.c

--- hash.c
+++ hash.c
@@ -1,1 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <stdbool.h>
+
+typedef struct hash_campo{
+	char* clave;
+	char* valor;
+} hash_campo_t;
+
+typedef struct hash{
+	hash_campo_t* tabla;
+	size_t tam;
+	size_t cant;
+} hash_t;
 
