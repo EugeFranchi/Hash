9a1d63c4336c14edc922769a4c0ff46ae7710b50
EugeFranchi <eugefranx@gmail.com>
2017-10-01 18:51:00 -0300

    Update hash.c

--- hash.c
+++ hash.c
@@ -1,11 +1,14 @@
+#include "hash.h"
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 #include <stdbool.h>
 
+#define TAMANIO_INICIAL 10
+
 typedef struct hash_campo{
-	char* clave;
-	char* valor;
+	void* clave;
+	void* valor;
 } hash_campo_t;
 
 typedef struct hash{
@@ -13,4 +16,25 @@ typedef struct hash{
 	size_t tam;
 	size_t cant;
 } hash_t;
+
+/* ************************************************************
+ *                             HASH
+ * ***********************************************************/ 
+hash_t *hash_crear(hash_destruir_dato_t destruir_dato){
+	hash_t* hash = malloc( sizeof(hash_t));
+	if (!hash){
+		return NULL;
+	}
+	
+	hash_campo_t* datos = malloc( sizeof(hash_campo_t) * TAMANIO_INICIAL);
+	if (!datos){
+		return NULL;
+	}
+	
+	hash->tabla = datos;
+	hash->cant = 0;
+	hash->tam = TAMANIO_INICIAL;
+	
+	return hash;
+}
 
