b8fa3daa696f6962ed01183c090358267b34970e
EugeFranchi <eugefranx@gmail.com>
2017-10-10 13:43:42 -0300

    agregue el iterador, me faltan hacer pruebas igual

--- hash.c
+++ hash.c
@@ -21,6 +21,11 @@ typedef struct hash{
 	hash_destruir_dato_t destruir_dato;
 } hash_t;
 
+struct hash_iter{
+	hash_campo_t* act;
+	hash_t* hash;
+};
+
 /* ************************************************************
  *                     FUNCIONES AUXILIARES
  * ***********************************************************/
@@ -37,13 +42,12 @@ size_t hashing (const char* clave, size_t tam){
 	return(clave_numerica);
 }
 
-
 //Devuelve la primera posicion vacia a partir de la posicion que arroja la funcion
 //si  no la encuentra devuelve -1
 int hallar_pos_vacia(hash_campo_t* tabla, size_t tam, int clave_h){
 	int pos = clave_h ;
-	 while(pos != clave_h -1){
-	 	if(tabla[pos].estado_campo == VACIO)
+	while(pos != clave_h -1){
+		if(tabla[pos].estado_campo == VACIO)
 	 		return pos ;
 	 	//cuando buscó hasta el final de la tabla vuelve a empezar desde el ppio
 	 	if(pos != tam-1)
@@ -52,6 +56,24 @@ int hallar_pos_vacia(hash_campo_t* tabla, size_t tam, int clave_h){
 	 		pos = 0 ;
 	 }
 	 return -1 ;
+}
+
+//Devuelve verdadero si esta vacio y false de lo contrario.
+bool hash_esta_vacio(const hash_t* hash){
+	return hash->cant == 0;
+}
+
+//Devuelve la siguiente posicion ocupada del hash, o -1 en caso de no haber.
+int hallar_pos_ocupada(const hash_t* hash, size_t inicio){
+	if (hash_esta_vacio(hash)){
+		return -1;
+	}
+	for (size_t pos = inicio; pos < hash->tam; pos++){
+		if(hash->tabla[pos].estado_campo == OCUPADO){
+			return pos;
+		}
+	}
+	return -1;
 }
 
 //Busca en la tabla la clave que le pasan por parametro
@@ -69,7 +91,6 @@ int posicion_clave(const hash_t* hash, const char*clave){
 }
 
 
-
 void destruir_contenido_tabla(hash_t* hash){
 	for(int i = 0 ; i < hash->tam ; i++){
 		if(hash->tabla[i].estado_campo == VACIO)
@@ -80,6 +101,7 @@ void destruir_contenido_tabla(hash_t* hash){
 		}
 	}
 }
+
 
 //Cambia el tamanio del hash por tam. Devuelve false si hay un error.
 //Pre: el hash fue creado.
@@ -100,7 +122,6 @@ bool hash_redimensionar(hash_t *hash, size_t tam){
 	free(tabla);
 	return true;
 }
-
 
 /* ************************************************************
  *                             HASH
@@ -197,3 +218,50 @@ bool hash_pertenece(const hash_t *hash, const char *clave){
 size_t hash_cantidad(const hash_t *hash){
 	return hash->cant ;
 }
+
+
+/* ************************************************************
+ *                         ITERADOR
+ * ***********************************************************/ 
+
+hash_iter_t *hash_iter_crear(const hash_t *hash){
+	hash_iter_t* iter = malloc(sizeof(hash_iter_t));
+	if (!iter){
+		return NULL;
+	}
+	
+	iter->act = &hash->tabla[hallar_pos_ocupada(hash, 0)];
+	*iter->hash = *hash;
+	return iter;
+}
+
+
+bool hash_iter_avanzar(hash_iter_t *iter){
+	if (hash_esta_vacio(iter->hash) || hash_iter_al_final(iter)){
+		return false;
+	}
+	
+	int pos = posicion_clave(iter->hash, iter->act->clave);
+	iter->act = &iter->hash->tabla[hallar_pos_ocupada(iter->hash, pos+1)];
+	return true;
+}
+
+
+const char *hash_iter_ver_actual(const hash_iter_t *iter){
+	if (iter->act){
+		return iter->act->clave;
+	}
+	return NULL;
+}
+
+
+bool hash_iter_al_final(const hash_iter_t *iter){
+	int pos = posicion_clave(iter->hash, iter->act->clave);
+	return hallar_pos_ocupada(iter->hash, pos+1) == -1;
+}
+
+
+void hash_iter_destruir(hash_iter_t* iter){
+	free(iter);
+}
+
