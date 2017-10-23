b9ac2403cd4658ecc9e17a47a4035fe3fccc4997
Andrea Gimenez <gim.andr26@gmail.com>
2017-10-22 22:03:27 -0300

    Update hash.c

--- hash.c
+++ hash.c
@@ -27,8 +27,8 @@ struct hash{
 };
 
 struct hash_iter{
-	hash_campo_t* act;
-	const hash_t* hash;
+	hash_t* hash;
+	size_t indice;
 };
 
 /* ************************************************************
@@ -36,39 +36,33 @@ struct hash_iter{
  * ***********************************************************/
 
 //Devuele una posicion segun la clave y el tamanio del hash.
-/*size_t hashing (const char* clave, size_t tam){
+size_t hashing (const char* clave, size_t tam){
 	unsigned int num1 = 378551;
 	unsigned int num2 = 63689;
 	unsigned int clave_numerica = 0;
+	unsigned int clave_como_int = *(unsigned int*)clave ;
 	for(int i = 0; *clave; clave++, i++){
-		clave_numerica = clave_numerica * num2 + (*clave);
+		clave_numerica = clave_numerica * num2 + clave_como_int;
 		num2 = num2 * num1;
 	}
 	return(clave_numerica%tam);
-}*/
-
-size_t hashing (const char* word, size_t hashtable_size){
-	int hash_adress = 0;
-	for (int counter = 0; word[counter] != '\0'; counter++){
-		hash_adress = word[counter] + (hash_adress << 6) + (hash_adress <<16) - hash_adress;
-	}
-	return (size_t) (hash_adress%hashtable_size);
 }
-
 //Devuelve la primera posicion vacia a partir de la posicion que arroja la funcion
 //si  no la encuentra devuelve -1
-int hallar_pos_vacia(hash_campo_t* tabla, size_t tam, int clave_h){
-	int pos = clave_h ;
-	while(pos != clave_h -1){
-		if(tabla[pos].estado_campo == VACIO)
-	 		return pos ;
+bool hallar_pos_vacia(hash_campo_t* tabla, size_t tam, size_t clave_h, size_t* pos_vacia){
+	size_t pos = clave_h ;
+	while(pos != clave_h-1){
+		if(tabla[pos].estado_campo == VACIO){
+	 		*pos_vacia = pos ;
+	 		return true ;
+		}
 	 	//cuando buscó hasta el final de la tabla vuelve a empezar desde el ppio
 	 	if(pos != tam-1)
 	 		pos ++ ;
 	 	else
 	 		pos = 0 ;
 	 }
-	 return -1 ;
+	 return false ;
 }
 
 //Devuelve verdadero si esta vacio y false de lo contrario.
@@ -77,30 +71,33 @@ bool hash_esta_vacio(const hash_t* hash){
 }
 
 //Devuelve la siguiente posicion ocupada del hash, o -1 en caso de no haber.
-int hallar_pos_ocupada(const hash_t* hash, size_t inicio){
+bool hallar_pos_ocupada(const hash_t* hash, size_t* pos_ocupada){
 	if (hash_esta_vacio(hash)){
-		return -1;
+		return false;
 	}
-	for (int pos = (int)inicio; pos < (int)hash->tam; pos++){
+	for (size_t pos = 0; pos < hash->tam; pos++){
 		if(hash->tabla[pos].estado_campo == OCUPADO){
-			return pos;
+			*pos_ocupada = pos ;
+			return true;
 		}
 	}
-	return -1;
+	return false;
 }
 
 //Busca en la tabla la clave que le pasan por parametro
 //si la encuentra, devuelve la posicion, sino devuelve -1
-size_t posicion_clave(const hash_t* hash, const char*clave){
-	int clave_hasheada = (int)hashing(clave, hash->tam) ;
-	for (int actual = clave_hasheada; hash->tabla[actual%hash->tam].estado_campo == OCUPADO; actual++){
-		if (hash->tabla[actual%hash->tam].estado_campo == BORRADO){
+bool posicion_clave(const hash_t* hash, const char*clave, size_t* pos){
+	size_t clave_hash = hashing(clave, hash->tam) ;
+	for (size_t actual = clave_hash; hash->tabla[actual].estado_campo == OCUPADO; actual++){
+		if (hash->tabla[actual].estado_campo == BORRADO){
 			continue;
 		}
-		if(strcmp(hash->tabla[actual%hash->tam].clave ,clave) == 0)
-			return actual%hash->tam ;
+		if(strcmp(hash->tabla[actual].clave ,clave) == 0){
+			*pos = actual ;
+			return true ;
+		}
 	}
-	return -1 ;
+	return false ;
 }
 
 
@@ -139,12 +136,25 @@ bool hash_redimensionar(hash_t *hash, size_t tam){
 			hash_guardar(hash, tabla_vieja[pos].clave, tabla_vieja[pos].valor);
 			free(tabla_vieja[pos].clave) ;
 		}
+		if(tabla_vieja[pos].estado_campo == BORRADO)
+			free(tabla_vieja[pos].clave) ;
 	}
 	hash->cant = cant;
 	free(tabla_vieja);
 	return true;
 }
 
+char* strdup(const char* str){
+	char* duplicado;
+	if(!str)
+		return NULL ;
+	size_t largo = strlen(str) ;
+	duplicado = malloc(sizeof(char) * (largo+1)) ;
+	if(!duplicado)
+		return NULL ;
+	strcpy(duplicado,str) ;
+	return duplicado ;
+}
 
 /* ************************************************************
  *                             HASH
@@ -179,19 +189,19 @@ bool hash_guardar(hash_t *hash, const char *clave, void *dato){
 	if (!clave){
 		return false;
 	}	
-	int pos_clave = posicion_clave(hash, clave) ;
+	size_t pos_clave;
+	char* clave_copia = strdup(clave) ; 
 	
-	if(pos_clave == -1){
-		int clave_hasheada = (int)hashing(clave, hash->tam) ;
-		int pos_vacia = hallar_pos_vacia(hash->tabla, hash->tam, clave_hasheada) ;
+	if(!posicion_clave(hash, clave_copia, &pos_clave)){
+		size_t clave_hasheada = hashing(clave_copia, hash->tam) ;
+		size_t pos_vacia ;
+		if(!hallar_pos_vacia(hash->tabla, hash->tam, clave_hasheada, &pos_vacia))
+			return false ;
 		
-		if(pos_vacia == -1)
-			return false ;///////creo que aca nunca llega si se redimensiona antes
-		
-		hash->tabla[pos_vacia].clave = malloc(sizeof(char) * strlen(clave)+1) ;
+		hash->tabla[pos_vacia].clave = malloc(sizeof(char) * strlen(clave_copia)+1) ;
 		if(!hash->tabla[pos_vacia].clave)
 			return false ;
-		strcpy(hash->tabla[pos_vacia].clave,clave);
+		strcpy(hash->tabla[pos_vacia].clave,clave_copia);
 		hash->tabla[pos_vacia].valor = dato ;
 		hash->tabla[pos_vacia].estado_campo = OCUPADO ;
 		hash->cant++;
@@ -211,32 +221,33 @@ bool hash_guardar(hash_t *hash, const char *clave, void *dato){
 
 void *hash_borrar(hash_t *hash, const char *clave){
 	if(!clave){
+		printf("clave nula en borrar \n") ;
 		return NULL;
 	}
-	int pos = posicion_clave(hash, clave) ;
-	if(pos == -1)
+	size_t pos;
+	if(!posicion_clave(hash, clave, &pos)){
+		printf("no se encontro posicion clave en borrar \n") ;
 		return NULL ;
+	}
 	//la clave existe en la tabla
-
 	void* dato = hash->tabla[pos].valor ;
 	hash->tabla[pos].estado_campo = BORRADO ;
 	hash->cant -- ;
-	if ((hash->cant < (double)hash->tam * CRITERIO_ACHICAR) && (hash->tam * FACTOR_ACHICAR >= TAMANIO_INICIAL)){
-		double nuevo_tam = (double) hash->tam * FACTOR_ACHICAR ;
-		hash_redimensionar(hash, (size_t)nuevo_tam);
+	if ((hash->cant < (double)hash->tam * CRITERIO_ACHICAR) && (hash->tam != TAMANIO_INICIAL)){
+		size_t nuevo_tam = (size_t)((float)hash->tam * FACTOR_AUMENTO) ;
+		if(!hash_redimensionar(hash, nuevo_tam))
+			return NULL ;
 	}
 	return dato ;
 }
-
 
 void *hash_obtener(const hash_t *hash, const char *clave){
 	if(!clave){
 		return NULL;
 	}
-	int pos = posicion_clave(hash, clave) ;
-	if(pos == -1)
+	size_t pos ; 
+	if(!posicion_clave(hash, clave, &pos))
 		return NULL ;
-
 	return hash->tabla[pos].valor ;
 }
 
@@ -244,8 +255,8 @@ bool hash_pertenece(const hash_t *hash, const char *clave){
 	if(!clave){
 		return NULL;
 	}
-	int pos = posicion_clave(hash, clave) ;
-	if(pos == -1)
+	size_t pos ;
+	if(!posicion_clave(hash, clave, &pos))
 		return false ;
 	return true ;
 }
@@ -264,44 +275,34 @@ hash_iter_t *hash_iter_crear(const hash_t *hash){
 	if (!iter){
 		return NULL;
 	}
-	
-	if (hallar_pos_ocupada(hash, 0) == -1){
-		iter->act = NULL;
-	}
-	else{
-		iter->act = &hash->tabla[hallar_pos_ocupada(hash, 0)];
-	}
-	iter->hash = hash;
+	iter->hash = (hash_t*)hash ;
+	size_t pos = 0;
+	hallar_pos_ocupada(hash,&pos);
+		iter->indice = pos;
 	return iter;
 }
 
-
 bool hash_iter_avanzar(hash_iter_t *iter){
-	if (hash_esta_vacio(iter->hash) || hash_iter_al_final(iter)){
+	if (hash_iter_al_final(iter))
 		return false;
+	iter->indice ++ ;
+	while(iter->hash->tabla[iter->indice].estado_campo != OCUPADO && !hash_iter_al_final(iter)){
+		iter->indice ++ ;
 	}
-	
-	int pos = posicion_clave(iter->hash, iter->act->clave);
-	if (hallar_pos_ocupada(iter->hash, (size_t)pos+1) == -1){
-		iter->act = NULL;
-	}
-	else{
-		iter->act = &iter->hash->tabla[hallar_pos_ocupada(iter->hash, (size_t)pos+1)];
-	}
-	return true;
+	if(hash_iter_al_final(iter))
+		return false ;
+	return true ;
 }
 
-
 const char *hash_iter_ver_actual(const hash_iter_t *iter){
-	if (iter->act){
-		return iter->act->clave;
-	}
-	return NULL;
+	if(hash_iter_al_final(iter))
+		return NULL ;
+	return iter->hash->tabla[iter->indice].clave ;
 }
 
 
 bool hash_iter_al_final(const hash_iter_t *iter){
-	if (!iter->act){
+	if (iter->indice == ((iter->hash->tam)-1) || iter->hash->cant == 0){
 		return true;
 	}
 	return false;
@@ -311,3 +312,4 @@ bool hash_iter_al_final(const hash_iter_t *iter){
 void hash_iter_destruir(hash_iter_t* iter){
 	free(iter);
 }
+
